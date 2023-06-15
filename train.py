cc9b56f1c5192bb89be00223e12506af83304083
ykzou9 <125784131+ykzou9@users.noreply.github.com>
2023-06-15 14:39:36 +0800

    train.py

--- train.py
+++ train.py
@@ -119,7 +119,8 @@ def train(train_loader, model, optimizer, epoch):
 
         image, depth = sample_batched['image'], sample_batched['depth']
 
-        depth = depth.cuda(async=True)
+        # depth = depth.cuda(async=True)
+        depth = depth.cuda(async_=True)
         image = image.cuda()
 
 
